44df62ab627b02f8e0b05f5075dd306dfa26ed7c
konnoy <konnoy@craftone.jpn.org>
2018-08-05 22:53:49 +0900

    Change UDPResponder report for issue #2

--- udpResponder/sendRecvStatsSnapshot.go
+++ udpResponder/sendRecvStatsSnapshot.go
@@ -29,31 +29,36 @@ func newSendRecvStatsSnapshot(s *sendRecvStats) sendRecvStatsSnapshot {
 
 }
 
-func (s sendRecvStatsSnapshot) SubSendReport(l sendRecvStatsSnapshot) string {
+func (s sendRecvStatsSnapshot) reportBytesPkts(l sendRecvStatsSnapshot) string {
 	duration := s.timestamp.Sub(l.timestamp)
 	durationSec := float64(duration) / float64(time.Second)
 
 	sendBytes := s.sendBytes - l.sendBytes
 	sendPackets := s.sendPackets - l.sendPackets
-
-	return "TX" + s.report(durationSec, sendBytes, sendPackets)
-}
-
-func (s sendRecvStatsSnapshot) SubRecvReport(l sendRecvStatsSnapshot) string {
-	duration := s.timestamp.Sub(l.timestamp)
-	durationSec := float64(duration) / float64(time.Second)
-
 	recvBytes := s.recvBytes - l.recvBytes
 	recvPackets := s.recvPackets - l.recvPackets
 
-	return "RX" + s.report(durationSec, recvBytes, recvPackets)
+	return fmt.Sprintf("RX=>TX Bytes/pkts in %.1f sec : %s / %s => %s / %s",
+		durationSec,
+		humanize.IBytes(recvBytes), stats.FormatSIUint(recvPackets, "pkts"),
+		humanize.IBytes(sendBytes), stats.FormatSIUint(sendPackets, "pkts"))
 }
 
-func (s sendRecvStatsSnapshot) report(durationSec float64, bytes, packets uint64) string {
-	bps := float64(bytes*8) / durationSec
-	pps := float64(packets) / durationSec
-	return fmt.Sprintf(" in %.1f sec : %s / %s => %s / %s",
+func (s sendRecvStatsSnapshot) reportBpsPps(l sendRecvStatsSnapshot) string {
+	duration := s.timestamp.Sub(l.timestamp)
+	durationSec := float64(duration) / float64(time.Second)
+
+	sendBytes := s.sendBytes - l.sendBytes
+	sendBps := float64(sendBytes*8) / durationSec
+	sendPackets := s.sendPackets - l.sendPackets
+	sendPps := float64(sendPackets) / durationSec
+	recvBytes := s.recvBytes - l.recvBytes
+	recvBps := float64(recvBytes*8) / durationSec
+	recvPackets := s.recvPackets - l.recvPackets
+	recvPps := float64(recvPackets) / durationSec
+
+	return fmt.Sprintf("RX=>TX bps/pps    in %.1f sec : %s / %s => %s / %s",
 		durationSec,
-		humanize.IBytes(bytes), stats.FormatSIUint(packets, "pkts"),
-		stats.FormatSIFloat(bps, "bps"), stats.FormatSIFloat(pps, "pps"))
+		stats.FormatSIFloat(recvBps, "bps"), stats.FormatSIFloat(recvPps, "pps"),
+		stats.FormatSIFloat(sendBps, "bps"), stats.FormatSIFloat(sendPps, "pps"))
 }
